4506aefb58adb416e611fa4ad0349d14d91ca56f
Johnremy Agbor <125705923+remy01gh@users.noreply.github.com>
2023-08-18 21:02:34 +0100

    Netflix Clone First version

---
+++ src/Nav.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import './Nav.css';
+
+function Nav() {
+    const [show, handleShow] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 100) {
+                handleShow(true);
+            } else {
+                handleShow(false);
+            }
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
+    return (
+        <div className={`nav ${show && "nav__black"}`}>
+            <img className='nav__logo' src='https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/Netflix-Logo.png/800px-Netflix-Logo.png?20230726112108' alt='Netflix Logo' />
+
+            <img className='nav__avater' src='https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png' alt='Netflix Avatar' />
+        </div>
+    );
+}
+
+export default Nav;
